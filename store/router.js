651c26feb506dd2b56c7f67912971ed09f07a38f
atiwat.sut <atiwat.sut@boonthavorn.com>
2023-05-09 23:06:59 +0700

    first commit

--- store/router.js
+++ store/router.js
@@ -1,4 +1,5 @@
-const url = process.env.URL_CLIENT + '/api/';
+// const url = process.env.URL_CLIENT + '/api/';
+const url = process.env.NODE_ENV == 'development' ? process.env.URL_API_DEV + '/' : process.env.URL_API + '/';
 export default {
   me: {
     menu: url + 'menu',
